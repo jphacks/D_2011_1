c5e2d8e02fbdbb7a2f7b95df7184e70ca159a034
Tomoya Tanaka <tomoya113.code@gmail.com>
2020-11-21 17:16:25 +0900

    Initial commit

---
+++ StatusBarApp/StatusBarApp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  StatusBarApp
+//
+//  Created by tomoya tanaka on 2020/11/21.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ StatusBarApp/StatusBarApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  StatusBarApp
+//
+//  Created by tomoya tanaka on 2020/11/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
